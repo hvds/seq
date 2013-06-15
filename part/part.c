69e2f06673952ea5f2c00fb6edb6ebdd7855d004
Hugo van der Sanden <hv@crypt.org>
2013-06-15 08:57:52 +0100

    dpart: replace seenness with canonicalness for pruning (*BROKEN*)
    When recursing, skip if the current subset is not canonical on the
    assumption that the subset will appear canonically either earlier or
    later. This does away with the complexity and memory cost of storing
    what has been seen.
    
    Currently this misses some solutions for n>=4, because the canonicalization
    is not progressive: there are canonical C<k+1>-piece partials that have no
    canonical C<k>-piece predecessor. Fixing that will require a more complex
    canonicalization (which will probably be slower). We are fast right now
    though: we find 68,524 solutions (of 75,218) for n=4 in 1.1s.
    
    Memory is also no longer a problem now: we can spend 30ks finding the first
    350 million solutions for n=5, and the process grows only to 2.6MB.

--- part/part.c
+++ part/part.c
@@ -4,199 +4,215 @@
 #include "symmetries.h"
 #include "pieces.h"
 #include "clock.h"
+#include <assert.h>
 
-#define REPORT_MASK ((1 << 16) - 1)
+#define REPORT_MASK ((1 << 18) - 1)
 
-counter sym_result;
+counter sym_result = 0;
+counter all_result = 0;
 
 typedef struct step_s {
+	int parent;		/* step index of last preceding piece of different size */
+	int piece_size;	/* size of this piece */
+	int remain;		/* number of bits set in freevec */
+
 	set_t set;		/* the combined set of pieces so far */
 	vec_t freevec;	/* bits still free */
 	vec_t shape;	/* a rotated/reflected piece to insert */
+	sym_set_t* ss;	/* symmetries that map the set so far to itself */
 } step_t;
-step_t steps[NODES];
+step_t steps[NODES + 1];
 set_t* solution;
-seth_tree** solutions_seen;
+sym_set_t* sym_filters[NODES];
 
-void setup_stack(void) {
+void setup_steps(void) {
 	uint i;
+	step_t* step;
+	sym_set_t* full_ss;
+
 	solution = (set_t*)malloc(sizeof(set_t));
-	solutions_seen = (seth_tree**)malloc(NODES * sizeof(seth_tree*));
 	for (i = 0; i < NODES; ++i)
-		solutions_seen[i] = seth_new();
+		steps[i].ss = symset_new();
+
+	step = &(steps[0]);
+	vec_zero(&(step->freevec));
+	vec_not(&(step->freevec));
+	step->remain = NODES;
+	step->parent = 0;
+	step->piece_size = NODES + 1;
+	vec_zero(&(step->shape));
+	set_init(&(step->set), &(step->shape));
+
+	full_ss = steps[0].ss;
+	for (i = 0; i < sym_count; ++i)
+		full_ss->index[i] = i;
+	full_ss->count = sym_count;
 }
 
-void teardown_stack(void) {
+void teardown_steps(void) {
 	uint i;
 	for (i = 0; i < NODES; ++i)
-		seth_delete(solutions_seen[i]);
-	free(solutions_seen);
+		symset_delete(steps[i].ss);
 	free(solution);
 }
 
-void reset_solutions(void) {
-	uint i;
-	for (i = 0; i < NODES; ++i)
-		seth_reset(solutions_seen[i]);
-}
-
 /*
-  Given a set_t I<s>, transforms it to canonical form. This is the symmetry
-  mapping under which it sorts lexically first.
-  The canonical set is in I<solution> immediately after calling.
+  Given a set I<set>, returns C<TRUE> if it is in canonical form (ie if in
+  its current state it sorts lexically before any symmetry of itself), else
+  C<FALSE>. The set of symmetries I<ssfrom> are the only ones considered.
+  If canonical, I<ssto> is also set to the subset of the symmetries in
+  I<ssfrom> that map I<set> to itself.
+
+  FIXME: this currently uses a broken definition of 'canonical': we want
+  to prune the recursion at any point that we reach a noncanonical set,
+  but with the current definition we may have a noncanonical set that
+  will become canonical (and therefore should be recursed to) on adding
+  another piece. For n=4, the set aabbccd**e*effd* is an example.
+  To fix this, rather than a simple lexical compare of the string before
+  and after applying the mapping for a symmetry, compare piece by piece:
+  this ensures that canonicalness remains stable under the addition of
+  pieces as long as the first node of the new piece appears after the
+  first node of any existing piece.
 */
-void canonical_set(set_t* set, uint pieces) {
-	uint i, sym_i;
+int canonicalize(set_t* set, sym_set_t* ssfrom, sym_set_t* ssto) {
+	uint i, sym_i, ssend;
 	uint seen, mapping[NODES], source, dest;
 	sym_t* sym;
 
-	/* the set may not start in canonical form, so a simple set_copy() here
-	 * is not enough.
-	 */
-	sym_i = 0;
-	sym = sym_map(sym_i);
-	seen = 0;
-	memset(mapping, 0, sizeof(mapping));
-	for (i = 0; i < NODES; ++i) {
-		source = set->p[sym->map[i]];
-		solution->p[i] = source
-			? mapping[source - 1]
-				? mapping[source - 1]
-				: (mapping[source - 1] = ++seen)
-			: 0;
-	}
+	/* Make a copy with 0 mapped to NODES, so we sort the way we want to */
+	for (i = 0; i < NODES; ++i)
+		solution->p[i] = set->p[i] ? set->p[i] : NODES;
 
-	for (sym_i = 1; sym_i < sym_count; ++sym_i) {
-		sym = sym_map(sym_i);
-
-		/* while applying the map, at each step we either know it is worse
-		 * (and abort) or that it is better (in which case we go to the
-		 * accept state) or that it is identical so far. So we need no
-		 * scratch space for the result, only for the assignments.
-		 */
+	ssend = ssfrom->count;
+	ssto->count = 0;
+	for (sym_i = 0; sym_i < ssend; ++sym_i) {
+		sym = sym_map(ssfrom->index[sym_i]);
 		seen = 0;
 		memset(mapping, 0, sizeof(mapping));
-
 		for (i = 0; i < NODES; ++i) {
 			source = set->p[sym->map[i]];
 			dest = source
 				? mapping[source - 1]
 					? mapping[source - 1]
 					: (mapping[source - 1] = ++seen)
-				: 0;
-			if (dest > solution->p[i])
-				goto SYM_FAIL;
+				: NODES;
+			/* if this map sorts earlier, the original is not canonical */
 			if (dest < solution->p[i])
-				goto SYM_ACCEPT;
+				return 0;
+			/* if this map sorts later, it's fine, but not an identity */
+			if (dest > solution->p[i])
+				goto NOT_IDENTITY;
 		}
-		/* if we fall through it's identical, treat same as fail */
-	  SYM_FAIL:
-		continue;
-
-	  SYM_ACCEPT:
-		for ( ; i < NODES; ++i) {
-			source = set->p[sym->map[i]];
-			solution->p[i] = source
-				? mapping[source - 1]
-					? mapping[source - 1]
-					: (mapping[source - 1] = ++seen)
-				: 0;
-		}
+		/* if we fall through it's an identity, so preserve it */
+		ssto->index[ssto->count++] = ssfrom->index[sym_i];
+	  NOT_IDENTITY:
+		;
 	}
-}
-
-int check_solution(set_t* set, uint pieces) {
-	canonical_set(set, pieces);
-	if (seth_seen(solutions_seen[pieces - 1], solution) == SETH_EXISTS)
-		return 0;
-
-	fprint_set(stdout, solution, pieces);
-	printf("\n");
-	++sym_result;
-	if (! (sym_result & REPORT_MASK)) {
-		fprintf(stderr, "%llu: ", sym_result);
-		fprint_set(stderr, solution, pieces);
-		fprintf(stderr, " (%.2f)\n", GTIME);
-	}
+	/* if we get through the gauntlet, we are canonical */
+	assert(ssto->count > 0);
+	assert(0 == (sym_count % ssto->count));
 	return 1;
 }
 
-void try_recurse(
-	uint prev_level, uint remain, uint maxpiece
-) {
-	uint level = prev_level + 1;
-	uint size, piece_index, end_index, sym_index;
+void check_solution(uint level) {
 	step_t* step = &(steps[level]);
-	step_t* prev_step = &(steps[prev_level]);
-	piece_t* this_piece;
-	vec_t *prev_free, *this_free;
-	vec_t* this_shape = &(step->shape);
+	set_t combined;
+	uint i;
+	uint syms = sym_count / step->ss->count;
+	assert(0 == (sym_count % step->ss->count));
 
-	/* Handle size=1 separately: no point filling in the singles, just take
-	 * them as read. If this is not a new solution, we've seen this shape
-	 * before, so skip the rest.
+	++sym_result;
+	all_result += syms;
+
+	set_zero(&combined);
+	for (i = level; i > 0; i = steps[i].parent)
+		set_merge(&(steps[i].set), &combined, steps[i].parent);
+
+	fprintf(stdout, "(%u) ", syms);
+	fprint_set(stdout, &combined);
+	printf("\n");
+	if (! (sym_result & REPORT_MASK)) {
+		fprintf(stderr, "%llu: (%u) ", sym_result, syms);
+		fprint_set(stderr, &combined);
+		fprintf(stderr, " (%.2f)\n", GTIME);
+	}
+}
+
+inline int insert_piece(uint level, vec_t* piece) {
+	step_t* step = &(steps[level]);
+	step_t* prev = &(steps[level - 1]);
+	uint parent;
+	if (step->piece_size == prev->piece_size) {
+		parent = prev->parent;
+		set_append(&(step->set), &(prev->set), piece, level - parent);
+	} else {
+		parent = level - 1;
+		set_init(&(step->set), piece);
+	}
+	step->parent = parent;
+	return canonicalize(&(step->set), steps[parent].ss, step->ss);
+}
+
+void try_recurse(uint prev_level) {
+	uint level = prev_level + 1;
+	step_t* prev = &(steps[prev_level]);
+	step_t* step = &(steps[level]);
+	uint max_size = prev->remain < prev->piece_size
+			? prev->remain : prev->piece_size;
+	uint size;
+
+	/* save as a solution the contents of prev_step, assuming all remaining
+	 * free locations are filled with pieces of size 1.
 	 */
-	if (! check_solution(&(prev_step->set), level + remain))
-		return;
-	/* If we're full, there's nothing to do */
-	if (remain == 0)
-		return;
+	check_solution(prev_level);
 
-	prev_free = &(prev_step->freevec);
-	this_free = &(step->freevec);
-	size = (remain < maxpiece) ? remain : maxpiece;
-	for ( ; size > 1; --size) {
-		if (size == remain) {
+#ifdef REVERSE
+	for (size = max_size; size >= 2; --size) {
+#else
+	for (size = 2; size <= max_size; ++size) {
+#endif
+		uint pi, end_index;
+#ifdef NOPREP
+		if (prev_level == 0) {
+			prep_pieces(size);
+		}
+#endif
+		step->piece_size = size;
+		step->remain = prev->remain - size;
+		if (step->remain == 0) {
 			/* no point iterating over the pieces, only one can fit */
-			if (is_connected(prev_free)) {
-				set_append(&(step->set), &(prev_step->set), prev_free, level);
-				check_solution(&(step->set), level + 1);
-			}
+			if (is_connected(&(prev->freevec)))
+				if (insert_piece(level, &(prev->freevec)))
+					check_solution(level);
 			continue;
 		}
-		piece_index = piece_array[size];
 		end_index = piece_array[size + 1];
-		for ( ; piece_index < end_index; ++piece_index) {
-			this_piece = pieces_vec(piece_index);
-			for (sym_index = 0; sym_index < this_piece->ss->count; ++sym_index) {
-				apply_map2(sym_map(this_piece->ss->index[sym_index]), &(this_piece->v), this_shape);
-				if (! vec_contains(prev_free, this_shape))
-					continue;
-				vec_xor3(prev_free, this_shape, this_free);
-				set_append(&(step->set), &(prev_step->set), this_shape, level);
-				try_recurse(
-					level,			/* recursion depth */
-					remain - size,	/* remaining bits free */
-					size			/* max size for pieces */
+		for (pi = piece_array[size]; pi < end_index; ++pi) {
+			piece_t* this_piece = pieces_vec(pi);
+			sym_set_t* ss = this_piece->ss;
+			uint sym_c = ss->count;
+			uint sym_i;
+			for (sym_i = 0; sym_i < sym_c; ++sym_i) {
+				apply_map2(
+					sym_map(ss->index[sym_i]), &(this_piece->v), &(step->shape)
 				);
+				if (! vec_contains(&(prev->freevec), &(step->shape)))
+					continue;
+				vec_xor3(&(prev->freevec), &(step->shape), &(step->freevec));
+				if (!insert_piece(level, &(step->shape)))
+					continue;
+				try_recurse(level);
 			}
+		}
+		if (prev_level == 0) {
+			fprintf(stderr, "solutions %u: %llu/%llu (%.2f)\n",
+					size, sym_result, all_result, GTIME);
 		}
 	}
 }
-
-void try_first(uint first) {
-	uint piece_index, end_index;
-	step_t* step = &(steps[0]);
-	piece_t *this_piece;
-
-	piece_index = piece_array[first];
-	end_index = piece_array[first + 1];
-
-	for ( ; piece_index < end_index; ++piece_index) {
-		this_piece = pieces_vec(piece_index);
-		vec_copy(&(this_piece->v), &(step->shape));
-		vec_not2(&(this_piece->v), &(step->freevec));
-		set_init(&(step->set), &(step->shape));
-		try_recurse(
-			0,				/* recursion depth */
-			NODES - first,	/* remaining bits free */
-			first			/* max size for pieces */
-		);
-	}
-}
-
+ 
 void teardown(void) {
-	teardown_stack();
+	teardown_steps();
 	teardown_pieces();
 	teardown_sym_set();
 	teardown_symmetries();
@@ -210,7 +226,7 @@ void setup(void) {
 	setup_symmetries();
 	setup_sym_set();
 	setup_pieces();
-	setup_stack();
+	setup_steps();
 }
 
 int main(int argc, char** argv) {
@@ -237,29 +253,9 @@ int main(int argc, char** argv) {
 
 #ifndef PIECE_ONLY
 	t1 = TIMETHIS({
-#ifndef REVERSE
-		for (first = 1; first <= NODES; ++first) {
-#else
-		for (first = NODES; first > 0; --first) {
-#endif
-#ifdef NOPREP
-			t2 = TIMETHIS({
-				prep_pieces(first);
-			});
-			fprintf(stderr, "pieces %u: %u [%u symsets] (%.2f)\n",
-					first, piece_array[first + 1] - piece_array[first],
-					sym_set_count, t2);
-#endif
-			prev = sym_result;
-			t2 = TIMETHIS({
-	 			try_first(first);
-				reset_solutions();
-			});
-			fprintf(stderr, "solutions %u: %llu/%llu (%.2f)\n",
-					first, sym_result - prev, sym_result, t2);
-		}
+		try_recurse(0);
 	});
-	printf("%u: Total %llu (%.2f)\n", NBASE, sym_result, t1);
+	printf("%u: Total %llu/%llu (%.2f)\n", NBASE, sym_result, all_result, t1);
 #endif
 	teardown();
 	return 0;
--- part/set.h
+++ part/set.h
@@ -6,40 +6,9 @@
 #include "part.h"
 #include "vec.h"
 
-typedef unsigned int seth;
-
 typedef struct set_s {
 	unsigned char p[NODES];
 } set_t;
-
-typedef enum {
-	SETH_GROW = -1,		/* insert ok, tree depth has grown */
-	SETH_OK = 0,		/* insert ok, tree depth has not grown */
-	SETH_EXISTS = 1		/* no insert, element already exists */
-} seth_insert_t;
-
-typedef struct seth_s {
-	seth left;
-	seth right;
-	int balance;
-	uint ref;
-} seth_t;
-
-typedef struct seth_tree_s {
-	seth_t* setharena;
-	seth sha_size;
-	seth sha_used;
-	seth sha_root;
-	set_t* setarena;
-	uint sa_size;
-	uint sa_used;
-} seth_tree;
-
-seth_tree* seth_new(void);
-void seth_delete(seth_tree* tree);
-void seth_reset(seth_tree* tree);
-seth_tree* seth_dup(seth_tree* source);
-seth_insert_t seth_seen(seth_tree* tree, set_t* v);
 
 #ifndef IS_SET_C
 #define SET_INLINE extern inline
@@ -47,8 +16,19 @@ seth_insert_t seth_seen(seth_tree* tree, set_t* v);
 #define SET_INLINE inline
 #endif
 
+SET_INLINE void set_zero(set_t* set) {
+	memset(set, 0, sizeof(set_t));
+}
+
 SET_INLINE void set_copy(set_t* source, set_t* dest) {
 	memcpy(dest, source, sizeof(set_t));
+}
+
+SET_INLINE void set_merge(set_t* source, set_t* dest, uint offset) {
+	uint i;
+	for (i = 0; i < NODES; ++i)
+		if (source->p[i])
+			dest->p[i] = source->p[i] + offset;
 }
 
 SET_INLINE void set_init(set_t* set, vec_t* v) {
@@ -63,10 +43,10 @@ SET_INLINE void set_append(set_t* dest, set_t* source, vec_t* v, uint size) {
 	for (i = 0; i < NODES; ++i)
 		if (vec_testbit(v, i)) {
 			assert(dest->p[i] == 0);
-			dest->p[i] = size + 1;
+			dest->p[i] = size;
 		}
 }
 
-void fprint_set(FILE* stream, set_t* set, uint size);
+void fprint_set(FILE* stream, set_t* set);
 
 #endif /* seth.h */
