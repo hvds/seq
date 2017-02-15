b47d1cd02c1a75cc8ce9bcd418be6ecc38bbea3d
Hugo van der Sanden <hv@crypt.org>
2017-02-15 13:08:20 +0000

    nurikabe: faster select connected spanning cells in a x b grid (3)
    Check for solution before recursing: if we find one, we know all additional
    permutations we find from here will also be solutions; when we know how
    many there will be, we can short-circuit.
    
    This is about 40% faster, so estimates for (8, 6), (8, 7), (8, 8) are now
    respectively 18h, 80d, 26y.

--- nurikabe/cframe.c
+++ nurikabe/cframe.c
@@ -15,6 +15,8 @@ typedef unsigned long long vec_t;
 
 vec_t edge[4];
 vec_t nb[MAX_A * MAX_B];
+vec_t fullvec;
+
 vec_t set, unset, surface;
 
 #define v1 ((vec_t)1)
@@ -90,24 +92,19 @@ void init(int a, int b) {
                 *n |= vbit(i, j - 1);
             if (j < b - 1)
                 *n |= vbit(i, j + 1);
+            fullvec |= vbit(i, j);
         }
     }
 }
 
-void check_solution(void) {
-    int i, j;
-    for (i = 0; i < 4; ++i) {
-        if ((set & edge[i]) == (vec_t)0)
-            return;
-    }
-    /* all edges seen, it's a solution */
-    ++count;
+void record_solution(unsigned long long found) {
+    count += found;
 
 #ifdef DEBUG
     report();
     keep_line();
 #else
-    if ((count & 0x3ffffff) == 0)
+    if ((count & 0x3ffffff) < found)
         report();
 #endif
 }
@@ -121,18 +118,43 @@ void recurse(int i, int j) {
 
     set |= vbit(i, j);
     surface |= *neighbour(i, j);
-
     mask = surface & ~(set | unset);
+
+    /* Choice of starting position guarantees we touch edge[0] */
+    if ((set & edge[1]) && (set & edge[2]) && (set & edge[3])) {
+        /* all edges seen, it's a solution */
+
+        if ((surface | set | unset) == fullvec) {
+            /* all remaining permutations will be solutions: count the
+             * bits in the mask representing the remaining surface, and
+             * report 2 to that power.
+             */
+            vec_t var = mask;
+            int bits = 0;
+            while (var) {
+                var &= var - 1;
+                ++bits;
+            }
+            record_solution(1 << bits);
+            goto done;
+        }
+
+        record_solution(1);
+    }
+
     for (x = 0; x < a; ++x) {
         for (y = 0; y < b; ++y) {
             if (mask & vbit(x, y)) {
                 recurse(x, y);
                 unset |= vbit(x, y);
+                mask &= ~vbit(x, y);
+                if (!mask)
+                    goto done;
             }
         }
     }
-    check_solution();
 
+  done:
     surface = old_surface;
     unset = old_unset;
     set &= ~vbit(i, j);
