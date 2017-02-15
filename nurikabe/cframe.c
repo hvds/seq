f7f61fae2eb58c5714c169fd4d1dd82d367e1547
Hugo van der Sanden <hv@crypt.org>
2017-02-15 12:31:46 +0000

    nurikabe: faster select connected spanning cells in a x b grid (2)
    Change vec_t definition from char[] to ULL, and simply usage. Reduces
    times by around 3/8.

--- nurikabe/cframe.c
+++ nurikabe/cframe.c
@@ -6,42 +6,23 @@
 int clk_tck;
 int gtime;
 
-int a, b, ab;
+int a, b;
 unsigned long long count;
 
 #define MAX_A 8
 #define MAX_B 8
-#define SIZE_A 1
-typedef struct vec_s {
-    unsigned char c[SIZE_A * MAX_B];
-} vec_t;
+typedef unsigned long long vec_t;
 
 vec_t edge[4];
 vec_t nb[MAX_A * MAX_B];
 vec_t set, unset, surface;
 
-inline void vec_set(vec_t *v, int i, int j) {
-    v->c[j] |= (1 << i);
-}
-inline void vec_unset(vec_t *v, int i, int j) {
-    v->c[j] &= ~(1 << i);
-}
-inline int vec_test(vec_t *v, int i, int j) {
-    if (v->c[j] & (1 << i))
-        return 1;
-    return 0;
-}
-inline void vec_copy(vec_t *dest, vec_t *src) {
-    memcpy((void *)dest, (void *)src, sizeof(vec_t));
-}
-inline void vec_or(vec_t *dest, vec_t *src) {
-    int i, j;
-    for (i = 0; i < SIZE_A * MAX_B; ++i) {
-        dest->c[i] |= src->c[i];
-    }
-}
+#define v1 ((vec_t)1)
+#define voff(x, y) ((y) * MAX_A + (x))
+#define vbit(x, y) (v1 << voff(x, y))
+
 inline vec_t *neighbour(int i, int j) {
-    return &nb[j * a + i];
+    return &nb[voff(i, j)];
 }
 
 double difftime(clock_t t0, clock_t t1) {
@@ -64,6 +45,10 @@ void clear_line(void) {
 void unclear_line(int count) {
     line_count += count;
 }
+void keep_line(void) {
+    line_count = 0;
+    printf("\n");
+}
 void report(void) {
     int output = 0, i, j;
     clear_line();
@@ -72,8 +57,8 @@ void report(void) {
         output += printf(" ");
         for (j = 0; j < b; ++j) {
             output += printf("%c",
-                vec_test(&set, i, j) ? '1'
-                : vec_test(&unset, i, j) ? '0' : '.');
+                (set & vbit(i, j)) ? '1'
+                : (unset * vbit(i, j)) ? '0' : '.');
         }
     }
     fflush(stdout);
@@ -86,26 +71,25 @@ void init(int a, int b) {
     clk_tck = sysconf(_SC_CLK_TCK);
     gtime = curtime();
 
-    ab = a * b;
     for (i = 0; i < a; ++i) {
-        vec_set(&edge[0], i, 0);
-        vec_set(&edge[1], i, b - 1);
+        edge[0] |= vbit(i, 0);
+        edge[1] |= vbit(i, b - 1);
     }
     for (i = 0; i < b; ++i) {
-        vec_set(&edge[2], 0, i);
-        vec_set(&edge[3], a - 1, i);
+        edge[2] |= vbit(0, i);
+        edge[3] |= vbit(a - 1, i);
     }
     for (i = 0; i < a; ++i) {
         for (j = 0; j < b; ++j) {
             vec_t *n = neighbour(i, j);
             if (i > 0)
-                vec_set(n, i - 1, j);
+                *n |= vbit(i - 1, j);
             if (i < a - 1)
-                vec_set(n, i + 1, j);
+                *n |= vbit(i + 1, j);
             if (j > 0)
-                vec_set(n, i, j - 1);
+                *n |= vbit(i, j - 1);
             if (j < b - 1)
-                vec_set(n, i, j + 1);
+                *n |= vbit(i, j + 1);
         }
     }
 }
@@ -113,47 +97,45 @@ void init(int a, int b) {
 void check_solution(void) {
     int i, j;
     for (i = 0; i < 4; ++i) {
-        int edge_seen = 0;
-        for (j = 0; j < SIZE_A * MAX_B; ++j) {
-            if ((set.c[j] & edge[i].c[j]) != 0) {
-                edge_seen = 1;
-                break;
-            }
-        }
-        if (!edge_seen)
+        if ((set & edge[i]) == (vec_t)0)
             return;
     }
     /* all edges seen, it's a solution */
     ++count;
-    if ((count & 0x3ffffff) == 0) {
+
+#ifdef DEBUG
+    report();
+    keep_line();
+#else
+    if ((count & 0x3ffffff) == 0)
         report();
-    }
+#endif
 }
 
 void recurse(int i, int j) {
-    vec_t old_surface, old_unset;
+    vec_t old_surface, old_unset, mask;
     int x, y;
 
-    vec_set(&set, i, j);
-    vec_copy(&old_unset, &unset);
-    vec_copy(&old_surface, &surface);
-    vec_or(&surface, neighbour(i, j));
+    old_unset = unset;
+    old_surface = surface;
+
+    set |= vbit(i, j);
+    surface |= *neighbour(i, j);
+
+    mask = surface & ~(set | unset);
     for (x = 0; x < a; ++x) {
         for (y = 0; y < b; ++y) {
-            if (vec_test(&surface, x, y)
-                && !vec_test(&set, x, y)
-                && !vec_test(&unset, x, y)
-            ) {
+            if (mask & vbit(x, y)) {
                 recurse(x, y);
-                vec_set(&unset, x, y);
+                unset |= vbit(x, y);
             }
         }
     }
     check_solution();
 
-    vec_copy(&surface, &old_surface);
-    vec_copy(&unset, &old_unset);
-    vec_unset(&set, i, j);
+    surface = old_surface;
+    unset = old_unset;
+    set &= ~vbit(i, j);
 }
 
 int main(int argc, char** argv) {
@@ -170,7 +152,7 @@ int main(int argc, char** argv) {
 
     for (i = 0; i < a; ++i) {
         recurse(i, 0);
-        vec_set(&unset, i, 0);
+        unset |= vbit(i, 0);
     }
     clear_line();
     printf("(%.2fs) f(%d, %d) = %lld\n", difftime(gtime, curtime()), a, b, count);
