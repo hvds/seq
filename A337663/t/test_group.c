05015f709f66954796d2d81748520f6ee2d321ae
Hugo van der Sanden <hv@crypt.org>
2020-10-25 13:06:44 +0000

    A337663: test group functions

--- A337663/t/test_group.c
+++ A337663/t/test_group.c
@@ -121,13 +121,116 @@ void is_grouplist(grouplist_t *got, grouplist_t *expect, char *legend, ...) {
 
 void test_place(void) {
     group_t *g = _parse_group((pgroup_t){ 1, 1, all_sym, "2" });
-    group_t *got = group_place(g, (loc_t){ -1, -1 }, 3);
+
+    group_t *got = group_place(g, (loc_t){ -1, 0 }, 3);
     ref_group(got);
-    group_t *expect = _parse_group((pgroup_t){ 2, 2, 1 << yx, "3 0; 0 2" });
-    is_group(got, expect, "group_place above left");
-    unref_group(g);
+    group_t *expect = _parse_group((pgroup_t){ 2, 1, 1 << xY, "3; 2" });
+    is_group(got, expect, "group_place above");
     unref_group(got);
     unref_group(expect);
+
+    got = group_place(g, (loc_t){ 0, 1 }, 3);
+    ref_group(got);
+    expect = _parse_group((pgroup_t){ 1, 2, 1 << Xy, "2 3" });
+    is_group(got, expect, "group_place right");
+    unref_group(got);
+    unref_group(expect);
+
+    unref_group(g);
+}
+
+void test_place_with(void) {
+    group_t *g = _parse_group((pgroup_t){ 1, 1, all_sym, "2" });
+
+    grouplist_t *got = group_place_with(g, (loc_t){ -1, -1 }, 3, 2);
+    pgroup_t pexpect[6] = {
+        (pgroup_t){ 2, 3, 0, "1 3 0; 1 0 2" },
+        (pgroup_t){ 3, 3, 0, "1 0 0; 0 3 0; 1 0 2" },
+        (pgroup_t){ 3, 3, yx, "0 0 1; 0 3 0; 1 0 2" },
+        (pgroup_t){ 3, 3, 0, "1 0 0; 1 3 0; 0 0 2" },
+        (pgroup_t){ 3, 3, yx, "0 1 0; 1 3 0; 0 0 2" },
+        (pgroup_t){ 3, 3, 0, "0 0 1; 1 3 0; 0 0 2" },
+    };
+    grouplist_t *expect = new_grouplist(6);
+    for (int i = 0; i < 6; ++i)
+        expect->g[i] = _parse_group(pexpect[i]);
+    is_grouplist(got, expect, "group_place_with above left with 2");
+    free_grouplist(got);
+    free_grouplist(expect);
+
+    unref_group(g);
+}
+
+/*
+
+--g1--  --g2--  place  with 1s  reflect
+   *        *
+ 2      1 3       4       2       YX
+        1 .
+
+         xy  (nothing, groups clash)
+
+             1 1 . .   1 . 1 .   . 1 1 .
+. * . .  ->  . 4 . .   . 4 . .   . 4 . .  reflects to yX
+2 . 3 1  xY  2 . 3 1   2 . 3 1   2 . 3 1
+. . . 1      . . . 1   . . . 1   . . . 1
+
+1 . .        1 . . .   1 . . .   1 . . .
+1 3 .    ->  1 3 . 1   1 3 . 1   1 3 . .  reflects to Yx
+. . *    Xy  . . 4 1   . . 4 .   . . 4 1
+. 2 .        . 2 . .   . 2 . 1   . 2 . 1
+
+. . . 1      . . . 1
+. . 3 1  ->  1 . 3 1  reflects to YX
+. * . .  XY  . 4 . .
+2 . . .      2 . 1 .
+
+         yx  (nothing, groups clash)
+
+1 1 .        1 1 . .   1 1 . .   1 1 . .
+. 3 .    ->  . 3 . 1   . 3 . 1   . 3 . .  reflects to xY
+. . *    yX  . . 4 1   . . 4 .   . . 4 1
+. 2 .        . 2 . .   . 2 . 1   . 2 . 1
+
+             1 1 . .   1 . 1 .   . 1 1 .
+. * . .  ->  . 4 . .   . 4 . .   . 4 . .  reflects to Xy
+2 . 3 .  Yx  2 . 3 .   2 . 3 .   2 . 3 .
+. . 1 1      . . 1 1   . . 1 1   . . 1 1
+
+. . 1 1      . . 1 1
+. . 3 .  ->  1 . 3 .  reflects to XY
+. * . .  yx  . 4 . .
+2 . . .      2 . 1 .
+
+*/
+void test_coalesce(void) {
+    group_t *g1 = _parse_group((pgroup_t){ 1, 1, all_sym, "2" });
+    group_t *g2 = _parse_group((pgroup_t){ 2, 2, 0, "1 3; 1 0" });
+
+    grouplist_t *got = coalesce_group(
+        g1, (loc_t){ -1, 1 }, g2, (loc_t){ -1, 2 }, 4, 2
+    );
+    pgroup_t pexpect[7] = {
+        /* xY */
+        (pgroup_t){ 4, 4, 0, "1 1 0 0; 0 4 0 0; 2 0 3 1; 0 0 0 1" },
+        (pgroup_t){ 4, 4, 0, "1 0 1 0; 0 4 0 0; 2 0 3 1; 0 0 0 1" },
+        (pgroup_t){ 4, 4, 0, "0 1 1 0; 0 4 0 0; 2 0 3 1; 0 0 0 1" },
+        /* Xy */
+        (pgroup_t){ 4, 4, 0, "1 0 0 0; 1 3 0 1; 0 0 4 1; 0 2 0 0" },
+        (pgroup_t){ 4, 4, 0, "1 0 0 0; 1 3 0 1; 0 0 4 0; 0 2 0 1" },
+        (pgroup_t){ 4, 4, 0, "1 0 0 0; 1 3 0 0; 0 0 4 1; 0 2 0 1" },
+        /* YX */
+        (pgroup_t){ 4, 4, 0, "0 0 1 1; 1 0 3 0; 0 4 0 0; 2 0 1 0" },
+    };
+    grouplist_t *expect = new_grouplist(7);
+    for (int i = 0; i < 7; ++i)
+        expect->g[i] = _parse_group(pexpect[i]);
+    is_grouplist(got, expect, "group_coalesce with 2");
+    free_grouplist(got);
+    free_grouplist(expect);
+
+    unref_group(g1);
+    unref_group(g2);
 }
 
 int main(void) {
@@ -136,6 +239,8 @@ int main(void) {
     init_group();
 
     test_place();
+    test_place_with();
+    test_coalesce();
 
     finish_group();
     finish_sym();
