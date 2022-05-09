94d6df7c1825df63e42af3cac3259596fea8562f
Hugo van der Sanden <hv@crypt.org>
2022-05-09 19:50:33 +0100

    divrep: support allsqrtmod() and friends
    These functions are due to be in Math::Prime::Util, but until a new release
    makes them available we can use a copy of the pure-perl implementation.

--- divrep/lib/ModFunc.pm
+++ divrep/lib/ModFunc.pm
@@ -2,6 +2,7 @@ package ModFunc;
 use strict;
 
 use Math::GMP ();
+use Math::Prime::Util qw{ sqrtmod chinese factor_exp };
 use List::Util qw{ reduce };
 
 require Exporter;
@@ -10,7 +11,11 @@ our @EXPORT_OK = qw/
     quadres is_residue quadvec
     mod_combine
     gcd
+    allsqrtmod allsqrtmodfact allsqrtmodpk
 /;
+
+my $zero = Math::GMP->new(0);
+sub MBI { defined($_[0]) ? Math::GMP->new(@_) : undef }
 
 {
     my %q;
@@ -92,6 +97,79 @@ sub gcd {
     my $n0 = shift;
     $n0 = Math::GMP->new($n0) unless ref $n0;
     return reduce { $a->bgcd($b) } ($n0, @_);
+}
+
+=head2 allsqrtmodpk( $a, $p, $k )
+
+Given integer C<a>, prime C<p> and positive integer C<k>, return a list of
+the square roots C<x> of C<a mod p^k> having C<< 0 <= x < p^k >>. If no
+square root exists, an empty list is returned.
+
+=cut
+
+sub allsqrtmodpk {
+    my($a, $p, $k) = @_;
+    my $pk = $p ** $k;
+    unless ($a % $p) {
+        unless ($a % $pk) {
+            # if p^k divides a, we need the square roots of zero, satisfied by
+            # ip^j with 0 <= i < p^{floor(k/2)}, j = p^{ceil(k/2)}
+            my $low = $p ** ($k >> 1);
+            my $high = ($k & 1) ? ($low * $p) : $low;
+            return map $high * $_, 0 .. $low - 1;
+        }
+        my $a2 = $a / $p;
+        return () if $a2 % $p;      # p divides a, p^2 does not
+        my $pj = $pk / $p;
+        return map {
+            my $qp = $_ * $p;
+            map $qp + $_ * $pj, 0 .. $p - 1;
+        } allsqrtmodpk($a2 / $p, $p, $k - 2);
+    }
+    my $q = MBI(sqrtmod($a, $pk));
+    return () unless defined $q;
+    return +($q, $pk - $q) if $p != 2;
+    return +($q) if $k == 1;
+    return +($q, $pk - $q) if $k == 2;
+    my $pj = $pk / $p;
+    my $q2 = ($q * ($pj - 1)) % $pk;
+    return +($q, $pk - $q, $q2, $pk - $q2);
+}
+
+=head2 allsqrtmodfact( $a, $n, $f )
+
+Given integers C<a> and C<n>, and the factorization C<f> of C<n> as returned
+by C<factor_exp(n)>, return a list of the square roots C<x> of C<a mod n>
+having C<< 0 <= x < n >>. If no square root exists, an empty list is returned.
+
+=cut
+
+sub allsqrtmodfact {
+    my($a, $n, $f) = @_;
+    return +($zero) unless @$f;
+    my($p, $k) = @{ $f->[0] };
+    $p = MBI($p);
+    my @q = allsqrtmodpk($a, $p, $k);
+    return @q if @$f == 1;
+    my $pk = $p ** $k;
+    my $n2 = $n / $pk;
+    return map {
+        my $q2 = $_;
+        map MBI(chinese([ $q2, $n2 ], [ $_, $pk ])), @q;
+    } allsqrtmodfact($a, $n2, [ @$f[1 .. $#$f] ]);
+}
+
+=head2 allsqrtmod( $a, $n )
+
+Given integers C<a> and C<n>, return a list of the square roots C<x> of
+C<a mod n> having C<< 0 <= x < n >>. If no square root exists, an empty
+list is returned.
+
+=cut
+
+sub allsqrtmod {
+    my($a, $n) = @_;
+    return allsqrtmodfact($a, $n, [ factor_exp($n) ]);
 }
 
 1;
