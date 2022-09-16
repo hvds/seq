22a47061be722bc6b278ab42f605f2a6e49adb3e
Hugo van der Sanden <hv@crypt.org>
2022-09-16 16:01:11 +0100

    A337663: modify Perl code to investigate a variant
    The original allows us to place k anywhere the sum of the 8 neighbours
    is k; the variant allows us to place k where the sum of the neighbours
    is a multiple mk of k, m >= 1.

--- A337663/lib/Board.pm
+++ A337663/lib/Board.pm
@@ -49,8 +49,8 @@ sub try {
     my $groups = $self->groups;
 
     # try making a new group
-    if ($unused >= $k) {
-        my $next_unused = $unused - $k;
+    for (my $mult = 1; $mult * $k <= $unused; ++$mult) {
+        my $next_unused = $unused - $k * $mult;
         for my $group (Group->new_seed($k)) {
             my $next_groups = [ @$groups, $group ];
             ($best, $best_board) = $self->recurse(
@@ -63,30 +63,35 @@ sub try {
     for my $i (0 .. $#$groups) {
         my $gi = $groups->[$i];
         my $sums = $gi->sums;
+        my $maxsum = $#$sums;
 
         # try exact first
-        for my $locn (@{ $sums->[$k] // [] }) {
-            my $new_group = $gi->place($locn, $k);
-            my $next_groups = [ @$groups ];
-            $next_groups->[$i] = $new_group;
-            ($best, $best_board) = $self->recurse(
-                $unused, $next_groups, $best, $best_board,
-            );
+        for my $mult (1 .. int($maxsum / $k)) {
+            for my $locn (@{ $sums->[$k * $mult] // [] }) {
+                my $new_group = $gi->place($locn, $k);
+                my $next_groups = [ @$groups ];
+                $next_groups->[$i] = $new_group;
+                ($best, $best_board) = $self->recurse(
+                    $unused, $next_groups, $best, $best_board,
+                );
+            }
         }
 
         # try with unused
-        for my $diff (1 .. $unused) {
-            my $sum = $k - $diff;
-            last if $sum < 0;
-            my $locns = $sums->[$sum] // next;
-            my $next_unused = $unused - $diff;
-            for my $locn (@$locns) {
-                for my $new_group ($gi->place_with($locn, $k, $diff)) {
-                    my $next_groups = [ @$groups ];
-                    $next_groups->[$i] = $new_group;
-                    ($best, $best_board) = $self->recurse(
-                        $next_unused, $next_groups, $best, $best_board,
-                    );
+        for my $mult (1 .. int(($maxsum + $unused) / $k)) {
+            for my $diff (1 .. $unused) {
+                my $sum = $k * $mult - $diff;
+                last if $sum < 0;
+                my $locns = $sums->[$sum] // next;
+                my $next_unused = $unused - $diff;
+                for my $locn (@$locns) {
+                    for my $new_group ($gi->place_with($locn, $k, $diff)) {
+                        my $next_groups = [ @$groups ];
+                        $next_groups->[$i] = $new_group;
+                        ($best, $best_board) = $self->recurse(
+                            $next_unused, $next_groups, $best, $best_board,
+                        );
+                    }
                 }
             }
         }
@@ -95,27 +100,31 @@ sub try {
         for my $j ($i + 1 .. $#$groups) {
             my $gj = $groups->[$j];
             my $sumsj = $gj->sums;
-            for my $sumi (1 .. $k - 1) {
-                my $locnsi = $sums->[$sumi] // next;
-                my $need = $k - $sumi;
-                my $min = $need - $unused;
-                $min = 1 if $min < 1;
-                for my $sumj ($min .. $need) {
-                    my $locnsj = $sumsj->[$sumj] // next;
-                    my $use = $need - $sumj;
-                    my $next_unused = $unused - $use;
-                    for my $locni (@$locnsi) {
-                        for my $locnj (@$locnsj) {
-                            for my $new_group ($gi->coalesce(
-                                $locni, $gj, $locnj, $k, $use,
-                            )) {
-                                my $next_groups = [ @$groups ];
-                                $next_groups->[$i] = $new_group;
-                                splice @$next_groups, $j, 1;
-                                ($best, $best_board) = $self->recurse(
-                                    $next_unused, $next_groups,
-                                    $best, $best_board,
-                                );
+            my $maxsumj = $#$sumsj;
+            for my $mult (1 .. int(($maxsum + $maxsumj + $unused) / $k)) {
+                my $targ = $k + $mult;
+                for my $sumi (1 .. $targ - 1) {
+                    my $locnsi = $sums->[$sumi] // next;
+                    my $need = $targ - $sumi;
+                    my $min = $need - $unused;
+                    $min = 1 if $min < 1;
+                    for my $sumj ($min .. $need) {
+                        my $locnsj = $sumsj->[$sumj] // next;
+                        my $use = $need - $sumj;
+                        my $next_unused = $unused - $use;
+                        for my $locni (@$locnsi) {
+                            for my $locnj (@$locnsj) {
+                                for my $new_group ($gi->coalesce(
+                                    $locni, $gj, $locnj, $k, $use,
+                                )) {
+                                    my $next_groups = [ @$groups ];
+                                    $next_groups->[$i] = $new_group;
+                                    splice @$next_groups, $j, 1;
+                                    ($best, $best_board) = $self->recurse(
+                                        $next_unused, $next_groups,
+                                        $best, $best_board,
+                                    );
+                                }
                             }
                         }
                     }
